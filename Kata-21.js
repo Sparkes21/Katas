3719ae211d85cfe615de4ef95deab1956a25058e
Josh Sparkes <jshsparx@gmail.com>
2022-06-15 13:51:46 -0600

    added if statements

--- Kata-21.js
+++ Kata-21.js
@@ -1,5 +1,22 @@
 let prompt = require("prompt-sync")();
 
 // code below (replace this example)
-let answers = prompt("Guess a number: ");
-console.log("You answered: " + answers);
+
+let number = Math.floor(Math.random() * 100);
+let attempts = 0;
+let answer = prompt("Guess a number: ");
+if (answer < number) {
+  console.log("Too Low!")
+  attempts++;
+}
+if (answer > number) {
+  console.log("Too High!")
+  attempts++;
+}
+if (isNaN(answer)) {
+  console.log('Not a number! Try again!')
+}
+if (answer === number) {
+  console.log(`You got it! You took ${attempts} attempts!`)
+}
+console.log("You answered: " + answer);
