d180bb1fab21afa2c9280498396ebb2748a7bf74
Josh Sparkes <jshsparx@gmail.com>
2022-06-15 15:06:18 -0600

    basic game functionality

--- Kata-21.js
+++ Kata-21.js
@@ -2,21 +2,28 @@ let prompt = require("prompt-sync")();
 
 // code below (replace this example)
 
-let number = Math.floor(Math.random() * 100);
-let attempts = 0;
-let answer = prompt("Guess a number: ");
-if (answer < number) {
-  console.log("Too Low!")
-  attempts++;
+const number = Math.floor(Math.random() * 100);
+let attemptsArr = [];
+let guess;
+
+
+while (guess !== number) {
+  let guess = prompt("Guess a number between 1-100: ");
+  if (attemptsArr.includes(guess)) {
+    console.log("Already guessed.")
+  } else if (!isNaN(guess)) {
+    attemptsArr.push(guess);
+  } 
+
+  if (guess > answer) {
+    console.log('Too High!')
+  } else if (guess < answer) {
+    console.log('Too Low!')
+  } else if (isNaN(guess)) {
+    console.log('Not a number, try again!');
+  } else {
+    console.log(`Correct! You took ${attemptsArr.length} attempts.`)
+  }
 }
-if (answer > number) {
-  console.log("Too High!")
-  attempts++;
-}
-if (isNaN(answer)) {
-  console.log('Not a number! Try again!')
-}
-if (answer === number) {
-  console.log(`You got it! You took ${attempts} attempts!`)
-}
-console.log("You answered: " + answer);
+
+
