f29bc722a4fb855b5e5c3dc2c8ad787251188f5d
Josh Sparkes <jshsparx@gmail.com>
2022-06-22 15:49:40 -0600

    fixed so it only prints out once

--- pig-latin.js
+++ pig-latin.js
@@ -1,9 +1,9 @@
 
 function pigLatin(string) {
-  for (let i = 0; i < string.length; i++) {
+  
     let firstLetter = string.slice(1)
     console.log(firstLetter);
-  }
+  
 }
 
 pigLatin("hello");
