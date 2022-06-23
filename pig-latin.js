69850ac24a3e3f457d35e486b24936ab3568903f
Josh Sparkes <jshsparx@gmail.com>
2022-06-23 11:41:37 -0600

    basic functionality

--- pig-latin.js
+++ pig-latin.js
@@ -1,10 +1,32 @@
+let strings = process.argv.slice(2);
+let lastString = strings[strings.length - 1];
+let translatedSentence = '';
 
-function pigLatin(string) {
-    let newString = string.split(' ')
-    console.log('newString', newString)
-    let firstLetter = newString.slice(1)
-    console.log(firstLetter + 'ay');
-  
+function pigLatin(strings) {
+  let translatedStrings = '';
+
+  for (let i = 1; i < strings.length; i++) {
+    translatedStrings += strings[i];
+  }
+
+  translatedStrings += strings[0];
+
+  return translatedStrings += strings == lastString ? 'ay' : 'ay'
+
 }
 
-pigLatin("pig latin");
+strings.forEach(function(strings) {
+  translatedSentence += pigLatin(strings);
+});
+
+console.log(translatedSentence);
+
+// function pigLatin(strings) {
+//     let newString = string.split(' ')
+//     console.log('newString', newString)
+//     let firstLetter = newString.slice(1)
+//     console.log(firstLetter + 'ay');
+  
+// }
+
+// pigLatin(string);
